40cf8c781526e0395059e069e6d438be9820567a
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-04-27 14:03:29 +0330

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>Amazon</header>
+      <main>Alaa</main>
+      <footer>all right reserved by Muhammad UFO</footer>
+    </div>
+  )
+}
+
+export default App
