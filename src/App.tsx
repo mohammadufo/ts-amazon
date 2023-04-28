6bed606d7d561aee2fd98200431bb316983b2e7d
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-04-28 18:30:44 +0330

    add products

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import './App.css'
+import {
+  Col,
+  Container,
+  Nav,
+  Navbar,
+  Row,
+} from '../node_modules/react-bootstrap/esm/index'
+import { sampleProducts } from './data'
 
 function App() {
   return (
-    <div>
-      <header>Amazon</header>
-      <main>Alaa</main>
-      <footer>all right reserved by Muhammad UFO</footer>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>Amazon</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+      <main>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All rights reserved by Muhammad UFO</div>
+      </footer>
     </div>
   )
 }
