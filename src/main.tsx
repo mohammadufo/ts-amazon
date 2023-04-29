80996de009ac8959b9a7c2f09094f1a0c2be960a
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-04-29 20:40:31 +0330

    fetch products

--- src/main.tsx
+++ src/main.tsx
@@ -11,14 +11,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.tsx'
 import './index.css'
 import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
+import axios from 'axios'
+
+// axios.defaults.baseURL = process.env.NODE_ENV = 'development'
+//   ? 'http://localhost:4000'
+//   : '/'
+
+axios.defaults.baseURL = 'http://localhost:4000'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} element={<HomePage />} />
-      {/* <Route path="product/:slug" element={<ProductPage />} /> */}
-      {/* <Route path="dashboard" element={<Dashboard />} /> */}
-      {/* ... etc. */}
+      <Route path="product/:slug" element={<ProductPage />} />
     </Route>
   )
 )
---
+++ src/pages/ProductPage.tsx
@@ -0,0 +1,9 @@
+const ProductPage = () => {
+  return (
+    <div>
+      <span>i love alaa . but allaho akbar</span>
+    </div>
+  )
+}
+
+export default ProductPage
