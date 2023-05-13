fbc2b33895a80460d140556c959b442944fdc4ed
mohammadufo <shadow.m.s.313@gmail.com>
2023-05-13 16:01:41 +0330

    add payment page

--- src/Store.tsx
+++ src/Store.tsx
@@ -41,7 +41,8 @@ type Action =
   | { type: "CART_REMOVE_ITEM"; payload: CartItem }
   | { type: "USER_SIGNIN"; payload: UserInfo }
   | { type: "USER_SIGNOUT" }
-  | { type: "SAVE_SHIPPING_ADDRESS"; payload: ShippingAddress };
+  | { type: "SAVE_SHIPPING_ADDRESS"; payload: ShippingAddress }
+  | { type: "SAVE_PAYMENT_METHOD"; payload: string };
 
 function reducer(state: AppState, action: Action): AppState {
   switch (action.type) {
@@ -99,6 +100,11 @@ function reducer(state: AppState, action: Action): AppState {
           ...state.cart,
           shippingAddress: action.payload,
         },
+      };
+    case "SAVE_PAYMENT_METHOD":
+      return {
+        ...state,
+        cart: { ...state.cart, paymentMethod: action.payload },
       };
     default:
       return state;
